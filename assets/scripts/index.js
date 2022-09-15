7c41305c9d4b0745d01e71f6b6ed5c91c2182725
Francis Maina <sebastianfrancix@gmail.com>
2022-09-15 23:01:41 +0300

    adds styling to the buttons

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,6 @@
 // my leads handler
-let myLeads = [];
+let myLeads = ["http://127.0.0.1:5500/index.html", "vvvvv"];
+const unorderedList = document.getElementById("unordered-list");
 
 const userInput = document.getElementById("input");
 
@@ -10,3 +11,8 @@ inputBtn.addEventListener("click", () => {
   myLeads.push(userInput.value);
   console.log(myLeads);
 });
+
+//loop through myLeads array and rendering it to the unordered list
+for (let i = 0; i < myLeads.length; i++) {
+  unorderedList.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
