0fd8b997571dd26258352bf908c6345e18bcee3f
Francis Maina <sebastianfrancix@gmail.com>
2022-09-16 09:02:01 +0300

    adds styling to the rendered items

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,6 +9,8 @@ const inputBtn = document.getElementById("input-button");
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(userInput.value);
+  //clear input field
+  userInput.value = "";
   renderLeads();
 });
 
@@ -16,7 +18,13 @@ function renderLeads() {
   //loop through myLeads array and rendering it to the unordered list
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `
+        <li>
+            <a target ='_blank' href= '${myLeads[i]}'> ${myLeads[i]}</a>
+        </li>
+        `;
+
+    // console.log(listItems);
   }
   unorderedList.innerHTML = listItems;
 }
